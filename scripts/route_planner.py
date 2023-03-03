7df69cb0174ed45d5f4d0ad441da12cae3f88d34
Fernando Cladera <fcladera@fcladera.com>
2023-03-02 19:41:36 -0500

    Heavy modifications to route_planner to be able to handle the sim

--- scripts/route_planner.py
+++ scripts/route_planner.py
@@ -11,88 +11,133 @@ import pprint
 import utm
 import numpy as np
 import heapq
+import rospy
+import rospkg
 
-SCALE = .3
+SCALE = 1
+
+""" Route planner uses standard coordinates (m) to plan the mission. These can be
+obtained from UTM (for GPS files) or as absolute coordinates for simulation
+files """
+
+
+class Mission():
+    """ Get a mission file and convert it to standard coordinates """
+    def __init__(self, mission_file, mission_file_format):
+        self.mission_file = mission_file
+        with open(mission_file, 'r') as f:
+            yml = yaml.load(f, Loader=yaml.FullLoader)
+
+        if mission_file_format == "QGC":
+            self.offset = np.array([0, 0])
+            # Check that the file is a valid QGC mission file
+            if (yml['fileType'] != "Plan" or
+                    yml['version'] != 1 or
+                    yml['groundStation'] != "QGroundControl"):
+                sys.exit(f"Error: {mission_file} is not a valid QGC plan file")
+
+            # Get mission in utm
+            mission = yml["mission"]["items"]
+            for m in mission:
+                if m["params"][4] is not None and m["params"][5] is not None:
+                    m["params"][4:6] = utm.from_latlon(m["params"][4],
+                                                       m["params"][5])[:2] - self.offset
+
+            # Get rally points in utm
+            self.rally = [utm.from_latlon(x, y)[:2]-self.offset
+                          for [x, y, _] in
+                          yml["rallyPoints"]["points"]]
+
+            # Extract waypoints from the mission
+            # Waypoints are a dictionary where the key is the waypoint numer,
+            # and the value is the standard coordinates
+            self.waypoints = {i: d["params"][4:6]
+                              for i, d in enumerate(mission)
+                              if d["command"] == 16}
+
+            # Get fence in UTM
+            self.fence = yml["geoFence"]["polygons"][0]
+            for p in self.fence["polygon"]:
+                p[0], p[1] = utm.from_latlon(p[0], p[1])[:2] - self.offset
+            if not self.fence["inclusion"]:
+                sys.exit("Error: the geoFence is not an inclusion zone")
+
+            # noFly is a list of polygons, with the coordinates of the noFly
+            # zones
+            self.noFly = [i["polygon"] for i in yml["geoFence"]["polygons"]
+                          if not i["inclusion"]]
+            for nf in self.noFly:
+                for p in nf:
+                    p[0], p[1] = utm.from_latlon(p[0], p[1])[:2] - self.offset
+        elif mission_file_format == "Sim":
+            self.noFly = [i["polygon"] for i in yml["geoFence"]["polygons"]
+                          if not i["inclusion"]]
+            self.rally = None
+            self.waypoints = yml["waypoints"]
+            self.fence = yml["geoFence"]["polygons"][0]
+            if not self.fence["inclusion"]:
+                sys.exit("Error: the geoFence is not an inclusion zone")
+
+        else:
+            sys.exit(f"Error: {mission_file_format} is not a valid format")
 
 
 class Path_planner():
-    def __init__(self, filename, image, offset_x, offset_y):
-        # Check that the file is a proper Path_planner file
-        with open(filename, 'r') as f:
-            yml = yaml.load(f, Loader=yaml.FullLoader)
-
-        # Checks to perform in the input file
-        if (yml['fileType'] != "Plan" or
-                yml['version'] != 1 or
-                yml['groundStation'] != "QGroundControl"):
-            sys.exit(f"Error: {filename} is not a valid QGC plan file")
+    def __init__(self, mission, map_name):
+        # Check that mission is a valid object
+        assert isinstance(mission, Mission)
+        self.mission = mission
 
         # The origin of the quad is an offset from the current utm position
-        self.offset = np.array([float(offset_x), float(offset_y)])
-
-        self.filename = filename
-        # Get mission in utm
-        self.mission = yml["mission"]["items"]
-        for m in self.mission:
-            if m["params"][4] is not None and m["params"][5] is not None:
-                m["params"][4:6] = utm.from_latlon(m["params"][4],
-                                                   m["params"][5])[:2] + self.offset
-
-        # Get rally points in utm
-        self.rally = [utm.from_latlon(x, y)[:2]+self.offset for [x, y, _] in
-                      yml["rallyPoints"]["points"]]
-        # Extract waypoints from the mission
-        self.waypoints = {i:d for i, d in enumerate(self.mission) if d["command"] == 16}
-
-        # Get fence in UTM
-        self.fence = yml["geoFence"]["polygons"][0]
-        for p in self.fence["polygon"]:
-            p[0], p[1] = utm.from_latlon(p[0], p[1])[:2] + self.offset
-        if not self.fence["inclusion"]:
-            sys.exit("Error: the geoFence is not an inclusion zone")
-
-        # Get no fly zones in UTM
-        self.noFly = [i for i in yml["geoFence"]["polygons"]
-                      if not i["inclusion"]]
-        for nf in self.noFly:
-            for p in nf["polygon"]:
-                p[0], p[1] = utm.from_latlon(p[0], p[1])[:2] + self.offset
+        # self.offset = np.array([float(offset_x), float(offset_y)])
+        self.offset = np.array([0, 0])
 
         # Store the last path for visualization purposes
         self.last_start = None
         self.last_end = None
         self.last_path = None
 
+        # Get the image from semantics_manager
+        pkg = rospkg.RosPack()
+        path = pkg.get_path('semantics_manager')
+        map_yaml_path = os.path.join(path, "maps", map_name, "map_config.yaml")
+        with open(map_yaml_path, 'r') as f:
+            map_yaml = yaml.load(f, Loader=yaml.FullLoader)
+        image = os.path.join(path, "maps", map_name, map_yaml["viz"])
+
+        # Get resolution (px/m) and image origin from the yaml
+        self.resolution = map_yaml["resolution"]
+        self.image_origin_px_x = map_yaml["image_origin_px_x"]
+        self.image_origin_px_y = map_yaml["image_origin_px_y"]
+
         # Origin of the image in UTM
-        img_lat, img_long = [39.943254, -75.202198]
-        self.img_utm = utm.from_latlon(img_lat, img_long)[:2] + self.offset
-        img_bottom_lat, img_bottom_long = [39.939762, -75.1982]
-        img_utm_bottom = utm.from_latlon(img_bottom_lat, img_bottom_long)[:2] + self.offset
-        self.corners_latlong = np.array([self.img_utm, img_utm_bottom ])
+        # img_lat, img_long = [39.943254, -75.202198]
+        # self.img_utm = utm.from_latlon(img_lat, img_long)[:2] - self.offset
+        # img_bottom_lat, img_bottom_long = [39.939762, -75.1982]
+        # img_utm_bottom = utm.from_latlon(img_bottom_lat, img_bottom_long)[:2] - self.offset
+        # self.corners_latlong = np.array([self.img_utm, img_utm_bottom ])
 
         # Keep picture for displaying purposes
         img = cv2.imread(image)
-        img = cv2.rotate(img, cv2.ROTATE_180)
-        img = cv2.flip(img, 1)
         self.img = cv2.resize(img, (0,0), fx=SCALE, fy=SCALE)
 
         # Get the scale of the image
-        pdb.set_trace()
-        self.scale_east = self.img.shape[1]/(img_utm_bottom[0] - self.img_utm[0])
-        self.scale_north = self.img.shape[0]/(img_utm_bottom[1] - self.img_utm[1])
-        print(f"Scale: {self.scale_east}, {self.scale_north}")
+        # self.scale_east = self.img.shape[1]/(img_utm_bottom[0] - self.img_utm[0])
+        # self.scale_north = self.img.shape[0]/(img_utm_bottom[1] - self.img_utm[1])
+        # print(f"Scale: {self.scale_east}, {self.scale_north}")
         self.generateGraph()
 
     def scale_points(self, utms_x, utms_y):
-        """ scale_points can take an utm point corresponding x,y coordinate in the image"""
+        """ scale_points can take an utm point and return the corresponding
+        x,y coordinates in the image"""
         if isinstance(utms_x, float):
             utms_x = [utms_x]
             utms_y = [utms_y]
         x = []
         y = []
         for utm_x, utm_y in zip(utms_x, utms_y):
-            x_, y_ = [int((utm_x - self.img_utm[0])*self.scale_east),
-                    int((utm_y - self.img_utm[1])*self.scale_north)]
+            x_, y_ = [int(utm_x*self.resolution)+self.image_origin_px_x,
+                      -int(utm_y*self.resolution)+self.image_origin_px_y]
             x.append(x_)
             y.append(y_)
         if len(x) == 1:
@@ -100,8 +145,8 @@ class Path_planner():
         return x, y
 
     def getDistance(self, p1, p2):
-        """Gets the distance in meters from two points p1 and p2, both points in
-        utm"""
+        """Gets the distance in meters from two points p0 and p2, both points in
+        standard coordinates """
         return np.linalg.norm(np.array(p1) - np.array(p2))
 
     def draw_poly_nofly(self, img, filled=False):
@@ -109,10 +154,10 @@ class Path_planner():
             color = 255
         else:
             color = (0, 0, 255)
-        for item in self.noFly:
-            lats = [i[0] for i in item["polygon"]]
-            longs = [i[1] for i in item["polygon"]]
-            x, y = self.scale_points(lats, longs)
+        for item in self.mission.noFly:
+            p1 = [i[0] for i in item]
+            p2 = [i[1] for i in item]
+            x, y = self.scale_points(p1, p2)
             # Close the polygon
             x.append(x[0])
             y.append(y[0])
@@ -127,9 +172,9 @@ class Path_planner():
     def generateGraph(self):
         # create a set with all the possible waypoints
         points = {}
-        for i in self.waypoints:
-            points[i] = {"latlon": self.waypoints[i]["params"][4:6],
-                         "neigh": {j: None for j in self.waypoints.keys() if
+        for i in self.mission.waypoints:
+            points[i] = {"latlon": self.mission.waypoints[i],
+                         "neigh": {j: None for j in self.mission.waypoints.keys() if
                                                     j != i}}
 
         # Remove points that are too far away from each other
@@ -142,6 +187,8 @@ class Path_planner():
         # Remove points that intersect with noFly zones
         polygon_mask = np.zeros(self.img.shape[:2], dtype=np.uint8)
         self.draw_poly_nofly(polygon_mask, filled=True)
+
+        # TODO: check that all the routes are within the fence
 
         for i in points:
             for j in points[i]["neigh"].copy():
@@ -175,7 +222,7 @@ class Path_planner():
 
         # Check that the points are within the allowed geofence
         fence_mask = np.zeros(self.img.shape[:2], dtype=np.uint8)
-        fence = np.array(self.fence["polygon"])
+        fence = np.array(self.mission.fence["polygon"])
         fence_x, fence_y = self.scale_points(fence[:, 0], fence[:, 1])
         fence = np.array([fence_x, fence_y]).T
         fence = fence.reshape((-1, 1, 2))
@@ -238,22 +285,16 @@ class Path_planner():
                 current_node = predecessors[current_node]
         return (dist, paths)
 
-    def display_points(self, mission=False, noFly=False, rally=False,
+    def display_points(self, waypoints=False, noFly=False, rally=False,
                        routes=False, plan=False):
         old_x = None
         old_y = None
         img = self.img.copy()
-        if mission:
-            for i, item in enumerate(self.mission):
-                if "params" not in item.keys():
-                    continue
-                lat = item['params'][4]
-                lon = item['params'][5]
-                # check if lat or lon are None
-                if lat is None or lon is None or lat == 0 or lon == 0:
-                    continue
+        if waypoints:
+            for i in self.mission.waypoints:
+                wp = self.mission.waypoints[i]
                 # print(f"Lat: {lat:.6f}, Lon: {lon:.6f}")
-                x, y = self.scale_points(lat, lon)
+                x, y = self.scale_points(wp[0], wp[1])
                 img = cv2.circle(img, (x, y), 2, (0, 0, 255), -1)
                 img = cv2.putText(img, str(i), (x, y),
                                   cv2.FONT_HERSHEY_SIMPLEX, .25, (0, 0, 255))
@@ -290,7 +331,7 @@ class Path_planner():
             if self.last_end is not None:
                 last_x_end, last_y_end = self.scale_points(self.last_end[0],
                                          self.last_end[1])
-                img = cv2.circle(img, (last_x_end, last_y_end), 
+                img = cv2.circle(img, (last_x_end, last_y_end),
                                  2, (0, 255, 0), -1)
 
             if self.last_start is not None:
@@ -324,29 +365,28 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
             prog=f"{os.path.basename(__file__)}",
             description='Read a yaml file and plot waypoints')
-    parser.add_argument('--filename', help='YAML file to read', required=True)
-    parser.add_argument('--image', help='Image to display', required=True)
-    parser.add_argument('--offset_x', help='UTM offset x', required=True)
-    parser.add_argument('--offset_y', help='UTM offset y', required=True)
+    parser.add_argument('--mission_file', help='YAML file with the mission', required=True)
+    parser.add_argument('--mission_file_format',
+                        help='Format of the mission file: QGC/Sim',
+                        required=True)
+    parser.add_argument('--map_name',
+                        help='Map name to use from semantics_manager',
+                        required=True)
     args = parser.parse_args()
 
     # Check if the file exists
-    if not os.path.isfile(args.filename):
-        sys.exit(f"Error: {args.filename} does not exist")
+    if not os.path.isfile(args.mission_file):
+        sys.exit(f"Error: {args.mission_file} does not exist")
 
-    # Check that image exists
-    if not os.path.isfile(args.image):
-        sys.exit(f"Error: {args.image} does not exist")
+    # Create a mission from the mission file
+    m = Mission(args.mission_file, args.mission_file_format)
 
-    # Check that image exists
-    q = Path_planner(args.filename, "../images/pennovation2.png", 
-                     args.offset_x, args.offset_y)
-
-    q.display_points(mission=True)
+    # Create a path planner object
+    q = Path_planner(m, args.map_name)
 
     # a = {r:{"neigh": list(q.graph[r]["neigh"].keys()),
     #         "utm": [str(j) for j in utm.from_latlon(q.graph[r]["latlon"][0],
-    #                                q.graph[r]["latlon"][1]) 
+    #                                q.graph[r]["latlon"][1])
     #                 ]} for r in q.graph}
     # pprint.pprint(a)
     # # save a into a yaml
@@ -356,15 +396,12 @@ if __name__ == "__main__":
 
     i = 0
     while (i < 10):
-        start_latlon = [random.uniform(q.corners_latlong[0, 0],
-                                       q.corners_latlong[1, 0]),
-                        random.uniform(q.corners_latlong[0, 1],
-                                       q.corners_latlong[1, 1])]
-        end_latlon = [random.uniform(q.corners_latlong[0, 0],
-                                     q.corners_latlong[1, 0]),
-                      random.uniform(q.corners_latlong[0, 1],
-                                     q.corners_latlong[1, 1])]
+        start_latlon = [random.uniform(-130, 130),
+                        random.uniform(-100, 100)]
+        end_latlon =   [random.uniform(-130, 130),
+                        random.uniform(-100, 100)]
+        print(f"Start: {start_latlon} - End: {end_latlon}")
         route = q.planRoute(start_latlon, end_latlon)
         if route is not None:
-            q.display_points(noFly=True, rally=True, routes=True, plan=True)
+            q.display_points(noFly=True, routes=True, plan=True)
             i += 1
