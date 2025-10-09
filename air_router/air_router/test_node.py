56694962fe45bda6288355a87b978248f8e0ab3a
JonD07 <jonathan.a.diller@gmail.com>
2025-10-09 15:13:27 -0400

    Initial move

---
+++ air_router/air_router/test_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from air_router.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ air_router/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'air_router'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jonathan',
+    maintainer_email='jonathan.a.diller@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'test_node = air_router.test_node:main'
+        ],
+    },
+)
